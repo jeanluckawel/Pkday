97ad2dc4e0d01bc7c6e525d0d1910f02e7bc7abf
JLK <20ka208@esisalama.org>
2023-05-24 22:08:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const txtAnim= document.querySelector('h5');
+new Typewriter(txtAnim, {
+    deleteSpeed: 150,
+    delay: 300
+})
+    .typeString('Heureux <span>Anniversaire</span> !')
+    .deleteChars(22)
+    .pauseFor(1000)
+    // .typeString('Paul kaf 84')
+    .typeString('1111 222 33')
+    .deleteChars(11)
+    .pauseFor(1000)
+    .typeString('Provite bien de ta journée')
+    .start()
